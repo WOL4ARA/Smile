8dbc29c242280488d8a2a5e3c9c620db18636f64
WOL4ARA <andriy.skavronskiy@gmail.com>
2019-08-13 19:29:01 +0300

    Create Module Customer

---
+++ Customer/Model/ResourceModel/PriceRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Smile\Customer\Model\ResourceModel;
+
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+
+/**
+ * Price request
+ *
+ * @package Smile\Customer\Model\ResourceModel\PriceRequest
+ */
+class PriceRequest extends AbstractDb
+{
+    /**
+     * Initialize resource model
+     *
+     * @return void
+     */
+    public function _construct()
+    {
+        $this->_init('smile_customer_price_request', 'id');
+    }
+}
